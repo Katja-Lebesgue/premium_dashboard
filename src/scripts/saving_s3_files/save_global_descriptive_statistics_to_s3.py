6fd8e3f6a96ddabea8a9cc0080f7f5beca03dee9
sanjin-lebesgue <sanjin@lebesgue.io>
2023-02-17 15:42:24 +0100

    save global descriptive by shop

--- src/scripts/saving_s3_files/save_global_descriptive_statistics_to_s3.py
+++ src/scripts/saving_s3_files/save_global_descriptive_statistics_to_s3.py
@@ -6,9 +6,10 @@ import pandas as pd
 import numpy as np
 
 from sqlalchemy import func
+from sqlalchemy.orm import Session
 
 from src.s3 import *
-from src.database.pingers import *
+from src.pingers import *
 from src.utils import *
 from metadata.globals import *
 from tqdm import tqdm
@@ -26,12 +27,13 @@ warnings.filterwarnings(
 
 @print_execution_time
 def save_global_descriptive_statistics_to_s3(
+    db: Session,
     folder=f"data/global/",
     bucket="creative-features",
     csv_file_name: str = None,
     end_date=date.today().strftime("%Y-%m-%d"),
+    force_from_scratch: bool = False,
 ):
-
     if csv_file_name is None:
         csv_file_name = f"global_descriptive_statistics_{end_date}"
 
@@ -39,7 +41,8 @@ def save_global_descriptive_statistics_to_s3(
 
     done_shop_ids_path = folder + csv_file_name + "_done_shop_ids" + ".csv"
 
-    all_shop_ids = ping_shops()["shop_id"]
+    shop_ids_query = db.query(AdCreativeFeatures.shop_id).distinct()
+    all_shop_ids = pd.read_sql(shop_ids_query.statement, db.bind)["shop_id"]
 
     group_cols = [
         "year_month",
@@ -48,26 +51,24 @@ def save_global_descriptive_statistics_to_s3(
     ]
     print(table_path)
     list_of_objects = list_objects_from_prefix(prefix=table_path)
+    from_scratch = len(list_of_objects) == 0 or force_from_scratch
 
     print(list_of_objects)
 
     # table was already created today
-    if len(list_of_objects):
+    if not from_scratch:
         # table was already created today
 
         table = read_csv_from_s3(path=table_path)
         table.set_index(keys=group_cols, inplace=True)
 
-        done_shop_ids = read_csv_from_s3(path=done_shop_ids_path, bucket=bucket)[
-            "shop_id"
-        ]
+        done_shop_ids = read_csv_from_s3(path=done_shop_ids_path, bucket=bucket)["shop_id"]
 
         print(f"we have {len(done_shop_ids)} done shop ids.")
 
         shop_ids = all_shop_ids[~all_shop_ids.isin(done_shop_ids)]
 
     else:
-
         table = pd.DataFrame()
         shop_ids = all_shop_ids
 
@@ -78,17 +79,13 @@ def save_global_descriptive_statistics_to_s3(
         "impr",
         "link_clicks",
         "purch",
-        "roas",
-        "cac",
     ]
 
     for shop_iter, shop_id in tqdm(enumerate(shop_ids), total=len(shop_ids)):
-
-        print(f"iter: {shop_iter}")
-
         print(f"shop_id: {shop_id}")
 
         data_shop = ping_creative_and_performance(
+            db=db,
             shop_id=shop_id,
             monthly=True,
             cast_to_date=False,
@@ -110,51 +107,44 @@ def save_global_descriptive_statistics_to_s3(
 
             save_csv_to_s3(df=done_shop_ids_df, bucket=bucket, path=done_shop_ids_path)
 
-            table.to_csv("./././data/probica.csv")
+            logger.debug(table)
 
-        if len(data_shop) == 0 or any(
-            [x not in data_shop.columns for x in ["spend_USD", "discounts_any"]]
-        ):
-            continue
-
-        # print(data_shop.columns)
-        # print(f"len: {len(data_shop)}")
-
-        data_shop.to_csv("./././data/global_description.csv")
-
-        if any([col not in data_shop.columns for col in descriptive_features]):
+        presence_of_cols = {col: col in data_shop.columns for col in descriptive_features + metric_columns}
+        if len(data_shop) == 0 or not all(presence_of_cols.values()):
+            logger.debug(
+                f"Skipam jer nemam {[col for col in presence_of_cols.keys() if presence_of_cols[col] is False]}"
+            )
             continue
 
         for col_iter, col in enumerate(descriptive_features):
-
-            grouped = (
-                data_shop.rename(columns={col: "feature_value"})
-                .groupby(
-                    [
-                        "year_month",
-                        "feature_value",
-                    ]
-                )
-                .sum()
-                .loc[:, metric_columns]
+            # for col_iter, col in enumerate(["target"]):
+            filtered = data_shop.loc[:, ["year_month", col] + metric_columns].rename(columns={col: "feature_value"})
+            sum_by_month = filtered.groupby("year_month").sum()
+            grouped = filtered.groupby(
+                [
+                    "year_month",
+                    "feature_value",
+                ]
             )
 
-            grouped["feature"] = col
-            grouped = grouped.set_index(["feature"], append=True)
-            grouped = grouped.reorder_levels(group_cols, axis=0)
+            absolute_sum = grouped.sum().loc[:, metric_columns]
+            relative_sum = (absolute_sum / sum_by_month).rename(columns=lambda col_name: col_name + "_by_shop")
 
-            grouped["count_shops"] = grouped.count_ads > 0
+            full = absolute_sum.join(relative_sum)
 
-            if shop_iter == 0 and col_iter == 0:
-                table = grouped
+            full["feature"] = col
+            full = full.set_index(["feature"], append=True)
+            full = full.reorder_levels(group_cols, axis=0).fillna(0)
+
+            logger.debug(f"full index: {full.index.names}")
+            logger.debug(f"table_index: {table.index.names}")
+
+            full["count_shops"] = (full.count_ads > 0).astype(int)
+
+            if shop_iter == 0 and col_iter == 0 and from_scratch:
+                table = full
             else:
-                table = table.add(grouped, fill_value=0)
-
-        print(f"minimal date in new shop: {data_shop.loc[:, 'year_month'].min()}")
-
-        print(
-            f"minimal date in table: {table.reset_index().loc[:, 'year_month'].min()}"
-        )
+                table = table.add(full, fill_value=0)
 
     save_csv_to_s3(
         df=table,
@@ -166,7 +156,6 @@ def save_global_descriptive_statistics_to_s3(
     done_shop_ids_df = pd.DataFrame(done_shop_ids, columns=["shop_id"])
     save_csv_to_s3(df=done_shop_ids_df, bucket=bucket, path=done_shop_ids_path)
 
-    table.to_csv("./././data/probica.csv")
     return table
 
 
