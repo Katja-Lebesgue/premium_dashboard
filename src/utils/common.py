51d9264c4e36b9a31e62a688f1dbbd6581252b51
sanjin-lebesgue <sanjin@lebesgue.io>
2023-08-09 13:57:46 +0000

    common utils

---
+++ src/utils/common.py
@@ -0,0 +1,17 @@
+import json
+import numpy as np
+
+
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NpEncoder, self).default(obj)
+
+
+def print_dict(d: dict, indent: int = 4):
+    print(json.dumps(d, indent=indent, cls=NpEncoder))
