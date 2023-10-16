215a2404b114b5beecd1eeefe922f73b56cbb6ad
sanjin-lebesgue <sanjin@lebesgue.io>
2023-10-16 11:38:47 +0000

    enum platform in ping_ads_insights

--- src/pingers/ping_ads_insights.py
+++ src/pingers/ping_ads_insights.py
@@ -13,6 +13,20 @@ from src.pingers.ping_crm import ping_crm
 from src.utils import convert_to_USD, element_to_list, read_query_into_df
 
 
+class Platform:
+    def __init__(self, name: str, ads_insights_model: DeclarativeMeta, account_model: DeclarativeMeta):
+        self.name = name
+        self.ads_insights_model = ads_insights_model
+        self.account_model = account_model
+
+
+facebook_platform = Platform(
+    name="facebook", ads_insights_model=FacebookAdsInsights, account_model=FacebookAdAccount
+)
+google_platform = Platform(name="google", ads_insights_model=GoogleAdsInsights, account_model=GoogleAdAccount)
+tiktok_platform = Platform(name="tiktok", ads_insights_model=TikTokAdsInsights, account_model=TikTokAdAccount)
+
+
 def ping_ads_insights_by_platform(
     model: DeclarativeMeta,
     account_model: DeclarativeMeta,
@@ -25,6 +39,7 @@ def ping_ads_insights_by_platform(
     end_date: date | str = date.today(),
     add_currency: bool = True,
     conversion_json: dict | None = None,
+    convert_to_datetime: bool = True,
 ) -> pd.DataFrame:
     if conversion_json is not None:
         add_currency = True
@@ -91,7 +106,7 @@ def ping_ads_insights_by_platform(
                 axis=1,
             )
 
-    if monthly:
+    if monthly and convert_to_datetime:
         df.year_month = df.year_month.apply(lambda x: datetime.strptime(x, "%Y-%m"))
 
     return df
@@ -99,40 +114,45 @@ def ping_ads_insights_by_platform(
 
 def ping_ads_insights_all_platforms(
     db: Session,
-    columns: str | list[str] = ["spend"],
+    columns: str | list[str] = ["spend", "revenue"],
     monthly: bool = True,
     shop_id: int | list[int] | None = None,
     start_date: date | str | None = None,
     end_date: date | str = date.today(),
     get_industry: bool = False,
     convert_to_USD_bool: bool = True,
+    pivot: bool = False,
+    convert_to_datetime: bool = True,
 ) -> pd.DataFrame:
     if convert_to_USD_bool:
         conversion_json = crud.currency_exchange_rate.ping_current_rates_dict(db=db)
 
-    for idx, (model, account_model) in enumerate(
-        zip(
-            [FacebookAdsInsights, TikTokAdsInsights, GoogleAdsInsights],
-            [FacebookAdAccount, TikTokAdAccount, GoogleAdAccount],
-        ),
-    ):
+    add_platform_prefix = not pivot
+    for idx, platform in enumerate((facebook_platform, google_platform, tiktok_platform)):
         new_df = ping_ads_insights_by_platform(
-            model=model,
-            account_model=account_model,
+            model=platform.ads_insights_model,
+            account_model=platform.account_model,
             db=db,
             columns=columns,
             monthly=monthly,
-            add_platform_prefix=True,
+            add_platform_prefix=add_platform_prefix,
             shop_id=shop_id,
             start_date=start_date,
             end_date=end_date,
             conversion_json=conversion_json,
+            convert_to_datetime=convert_to_datetime,
         )
+
+        if pivot:
+            new_df["platform"] = platform.name
 
         if idx == 0:
             df = new_df
         else:
-            df = df.merge(new_df, how="outer")
+            if pivot:
+                df = pd.concat([df, new_df], axis=0)
+            else:
+                df = df.merge(new_df, how="outer")
     if get_industry:
         crm = ping_crm()
         crm = crm[["shop_id", "industry"]]
@@ -140,9 +160,10 @@ def ping_ads_insights_all_platforms(
         df.industry = df.industry.apply(lambda x: x if type(x) == str else "unknown")
         df.industry = df.industry.replace({"Unknown": "unknown"})
 
-    for col in columns:
-        df[f"total_{col}"] = df.apply(
-            lambda df: np.nansum([df[f"{platform}_{col}"] for platform in PLATFORMS]), axis=1
-        )
+    if not pivot:
+        for col in columns:
+            df[f"total_{col}"] = df.apply(
+                lambda df: np.nansum([df[f"{platform}_{col}"] for platform in PLATFORMS]), axis=1
+            )
 
     return df
