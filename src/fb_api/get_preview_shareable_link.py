8e5311ec73b2f66a96adb9a82a44500e2a7bdbd2
Katja-Lebesgue <katja@lebesgue.io>
2024-02-28 11:28:33 +0100

    removed packages.txt

--- src/fb_api/get_preview_shareable_link.py
+++ src/fb_api/get_preview_shareable_link.py
@@ -14,8 +14,6 @@ from src.models import Credentials
 
 load_dotenv()
 
-"bla"
-
 def get_preview_shareable_link(
     ad_id: str, access_token: str | None, shop_id: int | None = None, db: Session | None = None
 ) -> pd.Series:
