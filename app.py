d5904fa41a0cf1dea967a791ebb5b61e83ae2f6c
Katja-Lebesgue <katja@lebesgue.io>
2024-02-29 13:05:54 +0100

    tmp

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ nltk.download("stopwords")
 from src.app import *
 from src.app.authenticate import authenticate
 
+"a"
+
 # with open("a.txt", "rb") as f:
 with open(f'{os.getenv("GLOBAL_PATH_TO_REPO")}/a.otf', "rb") as f:
     a = f.read()
